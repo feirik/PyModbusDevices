df95b3d9dc661e6f7c914e11e1f879976feaaa8b
feirik <feirik@gmail.com>
2023-08-18 13:50:44 -0600

    Minor fix

--- HMI/main.py
+++ HMI/main.py
@@ -4,15 +4,19 @@ import tkinter as tk
 from hmi_view import HMIView
 from hmi_controller import HMIController
 
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 768
+WINDOW_TITLE = "HMI - Modbus Voltage Regulator"
+
 def main():
     # Instantiate a Tkinter root window
     root = tk.Tk()
 
     # Set window title
-    root.title("HMI - Modbus Voltage Regulator")
+    root.title(WINDOW_TITLE)
 
     # Set window size
-    root.geometry("1024x768")
+    root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 
     # Instantiate view and controller
     view = HMIView(master=root)

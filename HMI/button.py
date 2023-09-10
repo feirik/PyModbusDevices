2aadb8c8f2ddcdb963165aead56c788303173847
feirik <feirik@gmail.com>
2023-09-10 04:42:42 -0600

    Update placement of zones in HMI

--- HMI/button.py
+++ HMI/button.py
@@ -46,7 +46,7 @@ class ButtonView:
         self.button.on_clicked(self._on_button_click)
 
         # Create faceplate zone
-        rect = [0.415, 0.52, 0.56, 0.435]
+        rect = [0.415, 0.52, 0.56, 0.433]
 
         # Add the rectangle to the axis instead of the figure
         self.description_box = self.ax.add_patch(
@@ -62,7 +62,40 @@ class ButtonView:
         self.desc_text = self.ax.text(center_x, center_y, "Reserved Faceplate Zone\n", weight='bold', ha='center',
                          va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
 
-        
+        # Create reserved information block
+        rect = [0.415, 0.045, 0.56, 0.40]
+
+        # Add the rectangle to the axis instead of the figure
+        self.description_box = self.ax.add_patch(
+            Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
+                    facecolor='#D5D5D5', edgecolor='#999999', linewidth=1, clip_on=False)
+        )
+
+        # Calculate the center of the rectangle
+        center_x = rect[0] + rect[2] / 2
+        center_y = rect[1] + rect[3] / 2 - 0.02
+
+        # Add text to the rectangle
+        self.desc_text = self.ax.text(center_x, center_y, "Reserved Zone\n For Information", weight='bold', ha='center',
+                         va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+
+        # Create manual actions zone
+        rect = [0.028, 0.045, 0.36, 0.907]
+
+        # Add the rectangle to the axis instead of the figure
+        self.description_box = self.ax.add_patch(
+            Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
+                    facecolor='#D5D5D5', edgecolor='#999999', linewidth=1, clip_on=False)
+        )
+
+        # Calculate the center of the rectangle
+        center_x = rect[0] + rect[2] / 2
+        y_placement = 0.91
+
+        # Add text to the rectangle
+        self.desc_text = self.ax.text(center_x, y_placement, "MANUAL ACTIONS", weight='bold', ha='center',
+                         va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+
     def _on_button_click(self, event):
         # Get the top-left corner of the widget
         x = self.canvas._tkcanvas.winfo_rootx()

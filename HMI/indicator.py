b6a8cacbbaf0272e077663f6b11c61a5da0c7d3e
feirik <feirik@gmail.com>
2023-10-07 13:51:30 -0600

    Use colors from common class

--- HMI/indicator.py
+++ HMI/indicator.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.patches import Rectangle
+from colors import HPHMI
 
 class Indicator:
     def __init__(self, master):
@@ -16,11 +17,12 @@ class Indicator:
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.grid(row=9, columnspan=4, rowspan=8, column=0, pady=20, padx=20)
 
+
     def setup_view(self):
         """Setup the Matplotlib figure and axis."""
         # Set the figure and axis background color
-        self.fig.patch.set_facecolor('#D5D5D5')
-        self.ax.set_facecolor('#D5D5D5')
+        self.fig.patch.set_facecolor(HPHMI.gray)
+        self.ax.set_facecolor(HPHMI.gray)
 
         # Remove axis labels and ticks
         self.ax.axis('off')
@@ -31,7 +33,7 @@ class Indicator:
         # Add the rectangle to the axis instead of the figure
         self.description_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         # Calculate the center of the rectangle
@@ -40,21 +42,21 @@ class Indicator:
 
         # Add text to the rectangle
         self.desc_text = self.ax.text(center_x, center_y, "Control Status", weight='bold', ha='center',
-                         va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+                         va='center', fontsize=10, color=HPHMI.darker_gray, transform=self.fig.transFigure)
 
         # Create a enable output description box
         rect = [0.022, 0.815, 0.32, 0.07]
 
         self.description_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         center_x = rect[0] + rect[2] / 2
         center_y = rect[1] + rect[3] / 2
 
         self.desc_text = self.ax.text(center_x, center_y, "ENABLE OUTPUT", weight='bold', ha='center',
-                                      va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+                                      va='center', fontsize=10, color=HPHMI.darker_gray, transform=self.fig.transFigure)
 
 
         # Create a enable output dynamic box
@@ -62,14 +64,14 @@ class Indicator:
 
         self.en_output_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         center_x = rect[0] + rect[2] / 2
         center_y = rect[1] + rect[3] / 2
 
         self.en_output_text = self.ax.text(center_x, center_y, "Loading...", weight='bold', ha='center',
-                         va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+                         va='center', fontsize=10, color=HPHMI.darker_gray, transform=self.fig.transFigure)
 
 
         # Create a enable override description box
@@ -77,14 +79,14 @@ class Indicator:
 
         self.description_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         center_x = rect[0] + rect[2] / 2
         center_y = rect[1] + rect[3] / 2
 
         self.desc_text = self.ax.text(center_x, center_y, "ENABLE OVERRIDE", weight='bold', ha='center',
-                         va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+                         va='center', fontsize=10, color=HPHMI.darker_gray, transform=self.fig.transFigure)
 
 
         # Create a enable override dynamic box
@@ -92,14 +94,14 @@ class Indicator:
 
         self.en_ovverride_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         center_x = rect[0] + rect[2] / 2
         center_y = rect[1] + rect[3] / 2
 
         self.en_override_text = self.ax.text(center_x, center_y, "Loading...", weight='bold', ha='center',
-                         va='center', fontsize=10, color='#4A4A4A', transform=self.fig.transFigure)
+                         va='center', fontsize=10, color=HPHMI.darker_gray, transform=self.fig.transFigure)
 
 
         # Create filler box
@@ -108,7 +110,7 @@ class Indicator:
         # Add the rectangle to the axis instead of the figure
         self.description_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         # Create filler box
@@ -117,12 +119,12 @@ class Indicator:
         # Add the rectangle to the axis instead of the figure
         self.description_box = self.ax.add_patch(
             Rectangle((rect[0], rect[1]), rect[2], rect[3], transform=self.fig.transFigure, 
-                    facecolor='#D5D5D5', edgecolor='#4A4A4A', linewidth=0.5, clip_on=False)
+                    facecolor=HPHMI.gray, edgecolor=HPHMI.darker_gray, linewidth=0.5, clip_on=False)
         )
 
         # Add an outline box around the entire figure
         outline_box = Rectangle((0, 0), 1, 1, transform=self.fig.transFigure, 
-                                facecolor='none', edgecolor='#999999', linewidth=2, clip_on=False)
+                                facecolor='none', edgecolor=HPHMI.dark_gray, linewidth=2, clip_on=False)
         self.fig.patches.extend([outline_box])
 
 
@@ -131,22 +133,22 @@ class Indicator:
         
         # Update 'enable_output' box and text
         if enable_output:
-            self.en_output_box.set_facecolor("#FFFFFF")
+            self.en_output_box.set_facecolor(HPHMI.white)
             self.en_output_text.set_text("ON")
-            self.en_output_text.set_color("#0000D7")
+            self.en_output_text.set_color(HPHMI.dark_blue)
         else:
-            self.en_output_box.set_facecolor("#999999")
+            self.en_output_box.set_facecolor(HPHMI.dark_gray)
             self.en_output_text.set_text("OFF")
-            self.en_output_text.set_color("#0000D7")
+            self.en_output_text.set_color(HPHMI.dark_blue)
 
         # Update 'enable_override' box and text
         if enable_override:
-            self.en_ovverride_box.set_facecolor("#FFFFFF")
+            self.en_ovverride_box.set_facecolor(HPHMI.white)
             self.en_override_text.set_text("ON")
-            self.en_override_text.set_color("#0000D7")
+            self.en_override_text.set_color(HPHMI.dark_blue)
         else:
-            self.en_ovverride_box.set_facecolor("#999999")
+            self.en_ovverride_box.set_facecolor(HPHMI.dark_gray)
             self.en_override_text.set_text("OFF")
-            self.en_override_text.set_color("#0000D7")
+            self.en_override_text.set_color(HPHMI.dark_blue)
 
         self.canvas.draw()
